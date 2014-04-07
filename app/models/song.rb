a74748917560645ed6ce701d6491a277a4f39648
Abigail Jones <AbbyJonesDev@gmail.com>
2014-04-07 17:15:14 -0500

    Song integration tests passing for basic actions

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
 
   has_attached_file :mp3
   validates_attachment :mp3,
-    :content_type => { :content_type => "audio/mp3" },
+    :content_type => { :content_type => ["audio/mp3", "audio/mpeg", "audio/mpeg3", "audio/x-mpeg-3"] },
     :file_name => { :matches => [/mp3\Z/] }
 
   has_attached_file :chords
