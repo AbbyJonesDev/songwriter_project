d6f083d6daf5b20d993d741160bd1a58213630e1
Abigail Jones <AbbyJonesDev@gmail.com>
2014-04-24 19:37:11 -0500

    Move admin login method to support file

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -100,14 +100,6 @@ feature "Manage resources from dashboard" do
   
   end
 
-def login_admin
-  @admin ||= FactoryGirl.create(:admin)
-  visit new_admin_session_path
-  click_on("Admin Login")
-  fill_in("Email", with: @admin.email)
-  fill_in("Password", with: @admin.password)
-  click_on("Sign in")
-end
 
   
 end
