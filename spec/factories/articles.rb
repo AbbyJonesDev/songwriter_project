5c25ee3e3aa9580cbbe6b8e4d69862191bb26c0f
Abigail Jones <AbbyJonesDev@gmail.com>
2014-04-25 22:41:42 -0500

    Sequence email in Admin factory and add association in Article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,10 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
+FactoryGirl.define do 
   factory :article do
     title "Article"
     content "Content of article"
-    admin_id 1
+    admin
     writer "Writer"
     keywords "keyword1, keyword2"
     published false
