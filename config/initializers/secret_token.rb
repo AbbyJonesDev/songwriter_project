615918530f3769db13f53869c0fb64f1c564761f
Abigail Jones <AbbyJonesDev@gmail.com>
2014-11-01 20:57:47 -0500

    Move secret token into ENV variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SongwriterProject::Application.config.secret_key_base = '6e2f6b7cae4f1e29edd78e8e55b3f1307693e85a86aab7a70faf5c90d242b13db2de50d0a152ae7e8fcb140be07b973ca2accd6de2ecdada4621d01a49752698'
+SongwriterProject::Application.config.secret_key_base = 
+  Rails.env.test? ? "83b0f863dc24531d93de0a4e3f2276" : ENV['SECRET_TOKEN']
